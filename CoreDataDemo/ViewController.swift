c4c67ca400031d3755826d8b0fab811f09b4b1ee
Valentina Abramova <valentisha@gmail.com>
2019-08-12 22:50:03 +0300

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Valentina Abramova on 12/08/2019.
+//  Copyright © 2019 Valentina Abramova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
